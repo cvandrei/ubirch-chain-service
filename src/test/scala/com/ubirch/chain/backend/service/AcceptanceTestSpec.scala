f2ab58482560a95dede910a35a96f8f113c0d068
cvandrei <crille@indarium.de>
2016-07-26 12:29:00 +0200

    acceptance tests: first draft

---
+++ src/test/scala/com/ubirch/chain/backend/service/AcceptanceTestSpec.scala
@@ -0,0 +1,38 @@
+package com.ubirch.chain.backend.service
+
+import com.typesafe.scalalogging.LazyLogging
+import org.scalatest.FeatureSpec
+
+/**
+  * author: cvandrei
+  * since: 2016-07-26
+  */
+class AcceptanceTestSpec extends FeatureSpec
+//  with Matchers // TODO enable when needed
+//  with GivenWhenThen // TODO enable when needed
+//  with ScalaFutures // TODO enable when needed
+  with LazyLogging {
+
+  feature("notify chainService about a new transaction") {
+
+    ignore("POST data (anything that can be represented as a string) and an external id") {
+      info("chainService responds with: (sha256(data), timestamp, externalId)")
+    }
+
+  }
+
+  feature("chain explorer") {
+
+    info("here we need the txHash result from notifying chainService about a new transaction")
+
+    ignore("GET txHash") {
+      info("chainService responds with: (txInfo(txHash, externalId, timeCreated), blockInfo(blockId, blockHash, timeCreated))")
+    }
+
+    ignore("GET externalId") {
+      info("chainService responds with: (txInfo(txHash, externalId, timeCreated), blockInfo(blockId, blockHash, timeCreated))")
+    }
+
+  }
+
+}
