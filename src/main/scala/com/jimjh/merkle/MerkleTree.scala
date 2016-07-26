002df50e3a761df2c04572471d622f7859b0bb5e
cvandrei <crille@indarium.de>
2016-07-26 16:33:53 +0200

    added merkle tree implementation by Jim Lim; including fix for unbalanced trees

---
+++ src/main/scala/com/jimjh/merkle/MerkleTree.scala
@@ -0,0 +1,51 @@
+package com.jimjh.merkle
+
+import java.util.Base64
+
+/** Hash of hashes, using a binary tree.
+  *
+  * The hash at each node is calculated using
+  * {{{
+  *   hash(hash of left + hash of right)
+  * }}}
+  *
+  * @constructor creates a new immutable merkle tree
+  * @param hash hash of children, or data block
+  * @author Jim Lim - jim@quixey.com (original source copied from https://github.com/jimjh/merkle-trees-impl)
+  * @author cvandrei - allow handling of unbalanced trees (e.g. only three leafs)
+  */
+class MerkleTree(val hash: Block,
+                 val left: Option[MerkleTree] = Option.empty,
+                 val right: Option[MerkleTree] = Option.empty) {
+
+  def this(hash: Block, left: MerkleTree, right: MerkleTree) =
+    this(hash, Option(left), Option(right))
+
+  override def toString =
+    s"MerkleTree(hash=${Base64.getEncoder.encodeToString(hash.toArray)}"
+}
+
+/** Companion object for [[MerkleTree]], acting as a factory. */
+object MerkleTree {
+
+  def apply(data: Seq[Block], digest: Digest): MerkleTree = {
+    var trees = data.map { block => new MerkleTree(digest(block)) }
+    while (trees.length > 1) {
+      trees = trees.grouped(2).map(make(digest, _)).toSeq
+    }
+    trees.head
+  }
+
+  private[this] def make(digest: Digest, arr: Seq[MerkleTree]): MerkleTree = {
+    val fixedLength = arr.length match {
+      case 2 => arr
+      case 1 => arr :+ arr.head
+    }
+    new MerkleTree(merge(digest, fixedLength), fixedLength.head, fixedLength(1))
+  }
+
+  private[this] def merge(digest: Digest, arr: Seq[MerkleTree]): Block = {
+    digest(arr.head.hash ++ arr(1).hash)
+  }
+
+}
---
+++ src/test/scala/com/jimjh/merkle/MerkleTreeSpec.scala
@@ -0,0 +1,117 @@
+package com.jimjh.merkle
+
+import java.nio.ByteBuffer
+import java.util.zip.CRC32
+
+import org.scalatest.{FeatureSpec, Matchers}
+
+/** Specs for [[com.jimjh.merkle.MerkleTree]].
+  *
+  * @author Jim Lim - jim@quixey.com
+  */
+class MerkleTreeSpec extends FeatureSpec
+  with Matchers {
+
+  feature("hashing a tree") {
+
+    scenario("where two (same tree twice) have same top hash") {
+      val data = Seq(
+        Seq[Byte](1, 2, 3),
+        Seq[Byte](4, 5, 6),
+        Seq[Byte](127, -128, -127),
+        Seq[Byte](117, 116, 118),
+        Seq[Byte](95, 94, 93),
+        Seq[Byte](125, 124, 123),
+        Seq[Byte](85, 84, 83),
+        Seq[Byte](75, 74, 73)
+      )
+      val tree1 = MerkleTree(data, crc32)
+      val tree2 = MerkleTree(data, crc32)
+      tree1.hash should be(tree2.hash)
+    }
+
+    scenario("where two have a different top hash") {
+      val data1 = Seq(
+        Seq[Byte](1, 2, 3),
+        Seq[Byte](4, 5, 6),
+        Seq[Byte](127, -128, -127),
+        Seq[Byte](117, 116, 118),
+        Seq[Byte](95, 94, 93),
+        Seq[Byte](125, 124, 123),
+        Seq[Byte](85, 84, 83),
+        Seq[Byte](75, 74, 73)
+      )
+
+      val data2 = Seq(
+        Seq[Byte](61, 2, 3),
+        Seq[Byte](64, 5, 6),
+        Seq[Byte](27, -128, -127),
+        Seq[Byte](17, 116, 118),
+        Seq[Byte](95, 94, 93),
+        Seq[Byte](125, 124, 123),
+        Seq[Byte](85, 84, 83),
+        Seq[Byte](75, 74, 73)
+      )
+
+      val tree1 = MerkleTree(data1, crc32)
+      val tree2 = MerkleTree(data2, crc32)
+      tree1.hash should not be tree2.hash
+    }
+
+    scenario("where two slightly different trees have the same child hash") {
+      val data1 = Seq(
+        Seq[Byte](1, 2, 3),
+        Seq[Byte](4, 5, 6),
+        Seq[Byte](127, -128, -127),
+        Seq[Byte](117, 116, 118),
+        Seq[Byte](95, 94, 93),
+        Seq[Byte](125, 124, 123),
+        Seq[Byte](85, 84, 83),
+        Seq[Byte](75, 74, 73)
+      )
+
+      val data2 = Seq(
+        Seq[Byte](61, 2, 3),
+        Seq[Byte](64, 5, 6),
+        Seq[Byte](27, -128, -127),
+        Seq[Byte](17, 116, 118),
+        Seq[Byte](95, 94, 93),
+        Seq[Byte](125, 124, 123),
+        Seq[Byte](85, 84, 83),
+        Seq[Byte](75, 74, 73)
+      )
+
+      val tree1 = MerkleTree(data1, crc32)
+      val tree2 = MerkleTree(data2, crc32)
+      tree1.right.get.hash should be(tree2.right.get.hash)
+      tree1.left.get.hash should not be tree2.left.get.hash
+    }
+
+    scenario("where tree has 5 leafs") {
+
+      val data = Seq(
+        Seq[Byte](1, 2, 3),
+        Seq[Byte](4, 5, 6),
+        Seq[Byte](127, -128, -127),
+        Seq[Byte](117, 116, 118),
+        Seq[Byte](95, 94, 93)
+      )
+
+      val expected = Seq[Byte](0, 0, 0, 0, -67, -71, 93, 84)
+
+      val tree = MerkleTree(data, crc32)
+      tree.hash should be(expected)
+
+    }
+
+  }
+
+  def crc32(b: Block) = {
+    val digest = new CRC32()
+    digest.update(b.toArray)
+
+    val buffer = ByteBuffer.allocate(8)
+    buffer.putLong(digest.getValue)
+    buffer.array
+  }
+}
