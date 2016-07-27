9c346889d8a21bf8b52d5be7c7c17f3214dcedd9
cvandrei <crille@indarium.de>
2016-07-27 18:07:51 +0200

    extract Json4sSupport to MyJsonProtocol

--- src/main/scala/com/ubirch/chain/backend/route/WelcomeRoute.scala
+++ src/main/scala/com/ubirch/chain/backend/route/WelcomeRoute.scala
@@ -2,27 +2,16 @@ package com.ubirch.chain.backend.route
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-
-import de.heikoseeberger.akkahttpjson4s.Json4sSupport
-
-import org.json4s._
-import org.json4s.ext.JavaTypesSerializers
+import com.ubirch.chain.json.{MyJsonProtocol, WelcomeMessage}
+import de.heikoseeberger.akkahttpjson4s.Json4sSupport._
 import org.json4s.jackson.JsonMethods.{parse => _}
 import org.json4s.jackson.Serialization.{write => _}
-
-import com.ubirch.chain.json.WelcomeMessage
 
 /**
   * author: cvandrei
   * since: 2016-07-27
   */
-class WelcomeRoute {
-
-  import Json4sSupport._
-
-  implicit val serialization = jackson.Serialization // or native.Serialization
-
-  implicit def json4sJacksonFormats: Formats = DefaultFormats ++ JavaTypesSerializers.all
+class WelcomeRoute extends MyJsonProtocol {
 
   val route: Route = {
 
--- src/main/scala/com/ubirch/chain/json/MyJsonProtocol.scala
+++ src/main/scala/com/ubirch/chain/json/MyJsonProtocol.scala
@@ -2,21 +2,15 @@ package com.ubirch.chain.json
 
 import org.json4s._
 import org.json4s.ext.JavaTypesSerializers
-import org.json4s.jackson.JsonMethods._
-import org.json4s.jackson.Serialization._
 
 /**
   * author: cvandrei
   * since: 2016-07-27
   */
-object MyJsonProtocol {
+trait MyJsonProtocol {
+
+  implicit val serialization = jackson.Serialization // or native.Serialization
 
   implicit def json4sJacksonFormats: Formats = DefaultFormats ++ JavaTypesSerializers.all
-
-  def toJsonFromObject(anyRef: AnyRef): JValue = Extraction.decompose(anyRef)
-
-  def toJsonFromString(json: String): JValue = parse(json)
-
-  def toStringJson(anyRef: AnyRef): String = write(anyRef)
 
 }
