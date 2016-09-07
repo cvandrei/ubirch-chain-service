3c775cf42c9d872708198a53b7e53a80afc8bf2e
cvandrei <crille@indarium.de>
2016-09-07 18:20:34 +0200

    add first non-working draft of adding the current anchor to previous blocks (still has a futures related compile problem)

--- core/src/main/scala/com/ubirch/chain/core/server/actor/AnchorUtil.scala
+++ core/src/main/scala/com/ubirch/chain/core/server/actor/AnchorUtil.scala
@@ -1,7 +1,7 @@
 package com.ubirch.chain.core.server.actor
 
 import com.typesafe.scalalogging.LazyLogging
-import com.ubirch.backend.chain.model.{Anchor, AnchorType}
+import com.ubirch.backend.chain.model.{Anchor, AnchorType, FullBlock}
 import com.ubirch.client.storage.ChainStorageServiceClient
 import com.ubirch.notary.client.NotaryClient
 
@@ -16,26 +16,34 @@ class AnchorUtil extends LazyLogging {
 
   def anchorNow(): Future[Boolean] = {
 
-    // TODO write automated tests
+    // TODO tests
     ChainStorageServiceClient.mostRecentBlock() map {
 
       case None =>
         logger.error("found no most recent block")
         false
 
-      case Some(block) => block.anchors.isEmpty match {
+      case Some(blockInfo) => blockInfo.anchors.isEmpty match {
 
         case false =>
           logger.info("most recent block has been anchored already")
           false
 
         case true =>
-          anchor(block.hash) match {
+          anchor(blockInfo.hash) match {
 
             case Some(anchor) =>
-              block.anchors :+ anchor
-              ChainStorageServiceClient.upsertBlock(block)
-              // TODO wait till upsert has been indexed --> otherwise we might anchor more than once
+              ChainStorageServiceClient.getFullBlock(blockInfo.hash) map {
+
+                case None =>
+
+                case Some(fullBlock) =>
+                  fullBlock.anchors :+ anchor
+                  ChainStorageServiceClient.upsertFullBlock(fullBlock)
+                  // TODO wait till upsert has been indexed --> otherwise we might anchor more than once
+                  anchorPreviousBlocks(fullBlock)
+
+              }
               true
 
             case _ => false // do nothing
@@ -48,8 +56,9 @@ class AnchorUtil extends LazyLogging {
 
   }
 
-  private def anchor(blockHash: String): Option[Anchor] = {
+  def anchor(blockHash: String): Option[Anchor] = {
 
+    // TODO tests
     logger.info(s"anchoring most recent blockHash: $blockHash")
 
     NotaryClient.notarize(blockHash, dataIsHash = true) match {
@@ -61,6 +70,50 @@ class AnchorUtil extends LazyLogging {
         Some(Anchor(anchorType, anchorHash))
 
       case None => None
+
+    }
+
+  }
+
+  def anchorPreviousBlocks(block: FullBlock) = {
+
+    // TODO tests
+    val previousBlocks = loadPreviousWithoutAnchor(block)
+    addAnchorToPreviousBlocks(previousBlocks, block.anchors)
+
+  }
+
+
+  def loadPreviousWithoutAnchor(block: FullBlock, list: Seq[FullBlock] = Seq.empty): Future[Seq[FullBlock]] = {
+
+    // TODO tests
+    ChainStorageServiceClient.getFullBlock(block.previousBlockHash) map {
+
+      case None => list
+
+      case Some(previous) =>
+        previous.anchors.isEmpty match {
+          case true => list
+          case false =>
+            loadPreviousWithoutAnchor(previous, list :+ previous)
+        }
+
+    }
+
+  }
+
+  private def addAnchorToPreviousBlocks(blocksWithoutAnchor: Future[Seq[FullBlock]], anchors: Seq[Anchor]): Unit = {
+
+    // TODO tests
+    blocksWithoutAnchor map { seq =>
+
+      seq foreach { block =>
+
+        block.anchors ++ anchors
+        ChainStorageServiceClient.upsertFullBlock(block)
+        Some(block)
+
+      }
 
     }
 
