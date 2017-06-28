b3a5dc82020608a6463b44ed63dfe300338525e9
cvandrei <crille@indarium.de>
2017-06-28 17:38:34 +0200

    UBI-252: add _id_ field to DeviceMsgHashIn

--- model-rest/src/main/scala/com/ubirch/chain/model/rest/DeviceMsgHashIn.scala
+++ model-rest/src/main/scala/com/ubirch/chain/model/rest/DeviceMsgHashIn.scala
@@ -4,4 +4,6 @@ package com.ubirch.chain.model.rest
   * author: cvandrei
   * since: 2017-06-28
   */
-case class DeviceMsgHashIn(hash: String)
+case class DeviceMsgHashIn(id: String,
+                           hash: String
+                          )
